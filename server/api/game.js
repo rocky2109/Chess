81163db89b03786ed11ab3361ef72a9b92e976a8
rocky2109 <rockys.2109rs@gmail.com>
2025-06-04 07:33:48 +0530

    Update game.js

--- server/api/game.js
+++ server/api/game.js
@@ -12,45 +12,23 @@ export default function handler(req, res) {
         io.on('connection', (socket) => {
             console.log('New client connected');
             
-            let gameId = null;
-            let playerColor = null;
-
-            socket.on('join', () => {
-                // Find or create a game
-                for (const id in games) {
-                    if (games[id].players < 2) {
-                        gameId = id;
-                        playerColor = 'black';
-                        games[gameId].players++;
-                        games[gameId].sockets.push(socket.id);
-                        break;
-                    }
+            socket.on('join', (data) => {
+                if (data.mode === 'friend') {
+                    handleFriendGame(socket);
                 }
-
-                if (!gameId) {
-                    gameId = Math.random().toString(36).substring(2, 8);
-                    playerColor = 'white';
-                    games[gameId] = {
-                        fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
-                        players: 1,
-                        sockets: [socket.id]
-                    };
-                }
-
-                socket.join(gameId);
-                socket.emit('color', playerColor);
-                socket.emit('gameState', games[gameId].fen);
             });
 
-            socket.on('move', (move) => {
-                if (gameId) {
+            socket.on('move', (data) => {
+                const { gameId, move } = data;
+                if (games[gameId]) {
                     games[gameId].fen = move.after;
                     socket.to(gameId).emit('move', move);
                 }
             });
 
-            socket.on('reset', () => {
-                if (gameId) {
+            socket.on('reset', (data) => {
+                const { gameId } = data;
+                if (games[gameId]) {
                     games[gameId].fen = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1';
                     io.to(gameId).emit('gameState', games[gameId].fen);
                 }
@@ -58,15 +36,45 @@ export default function handler(req, res) {
 
             socket.on('disconnect', () => {
                 console.log('Client disconnected');
-                if (gameId && games[gameId]) {
-                    games[gameId].players--;
-                    games[gameId].sockets = games[gameId].sockets.filter(id => id !== socket.id);
-                    
-                    if (games[gameId].players === 0) {
-                        delete games[gameId];
+                // Clean up games
+                for (const id in games) {
+                    games[id].sockets = games[id].sockets.filter(s => s !== socket.id);
+                    if (games[id].sockets.length === 0) {
+                        delete games[id];
                     }
                 }
             });
+
+            function handleFriendGame(socket) {
+                // Try to find existing game waiting for player
+                for (const id in games) {
+                    if (games[id].players < 2) {
+                        joinGame(socket, id, 'black');
+                        return;
+                    }
+                }
+
+                // Create new game
+                const newGameId = generateGameId();
+                games[newGameId] = {
+                    fen: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1',
+                    players: 1,
+                    sockets: [socket.id]
+                };
+                joinGame(socket, newGameId, 'white');
+            }
+
+            function joinGame(socket, gameId, color) {
+                socket.join(gameId);
+                games[gameId].players++;
+                games[gameId].sockets.push(socket.id);
+                socket.emit('gameCreated', { gameId, color });
+                socket.emit('gameState', games[gameId].fen);
+            }
+
+            function generateGameId() {
+                return Math.random().toString(36).substring(2, 8).toUpperCase();
+            }
         });
     }
     res.end();
